ab8df10820f2c79b372bb3e62daff7344a4d0823
Harry <harrychopra@gmail.com>
2023-02-17 03:00:00 -0800

    Variables & Operators

---
+++ Section2-Exercises/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+    }
+
+    // Variables and Operators
+    public static int VariablesAndOperators()
+    {
+        var a = 5;
+        var b = 10;
+        var c = 3;
+
+        int result = (a + b) / c;
+
+        return result;
+    }
+}
