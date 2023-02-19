cd226a0e87c16022339ee7e8e38850959fb22d8b
Harry <harrychopra@gmail.com>
2023-02-19 01:00:00 -0800

    Section2 Calculator

---
+++ Section2-Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿// Calculator
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello!");
+        Console.WriteLine("Input the first number:");
+        int numA = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Input the second number:");
+        int numB = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("What do you want to do?");
+        Console.WriteLine(
+            "[A]dd numbers\n" +
+            "[S]ubtract numbers\n" +
+            "[M]ultiply numbers");
+        char userChoice = char.Parse(Console.ReadLine());
+
+        switch (userChoice)
+        {
+            case 'a':
+            case 'A':
+                var sum = numA + numB;
+                PrintEquation(numA, numB, "+",sum);
+                break;
+            case 's':
+            case 'S':
+                var difference = numA - numB;
+                PrintEquation(numA, numB, "-", difference);
+                break;
+            case 'm':
+            case 'M':
+                var product = numA * numB;
+                PrintEquation(numA, numB, "*", product);
+                break;
+            default:
+                Console.WriteLine("Invalid choice!");
+                break;
+        }
+    }
+
+    private static void PrintEquation(int numA, int numB, string @operator, int result)
+    {
+        Console.WriteLine($"{numA} {@operator} {numB} = {result}");
+    }
+}
