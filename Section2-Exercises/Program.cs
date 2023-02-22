c807d8339d03915e0de91d35a2cbde6ce0741204
Harry <harrychopra@gmail.com>
2023-02-22 01:00:00 +0000

    Working with lists

--- Section2-Exercises/Program.cs
+++ Section2-Exercises/Program.cs
@@ -174,4 +174,28 @@
 
         return maxVal;
     }
+
+    public List<string> GetOnlyUpperCaseWords(List<string> words)
+    {
+        var result = new List<string>();
+
+        foreach (var word in words)
+        {
+            if (!result.Contains(word) && IsUpperCase(word)) result.Add(word);
+        }
+
+        return result;
+    }
+
+    public bool IsUpperCase(string word)
+    {
+        foreach (char character in word)
+        {
+            if (!char.IsUpper(character))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 }
